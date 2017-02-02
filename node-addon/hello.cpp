89b883b1349d38f08b5dcbd6ef9db800f48f2303
Rémy MEJA <remy.meja@inist.fr>
2017-02-02 08:12:45 +0100

    Adding an experimentation about node-addon

---
+++ node-addon/hello.cpp
@@ -0,0 +1,15 @@
+// hello.cc
+#include <node.h>
+
+using namespace v8;
+
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+}
+
+void init(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "hello", Method);
+}
+
+NODE_MODULE(addon, init)
