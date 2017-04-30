bc92e21c0223c71ea76cedc53759acba022992af
Rémy MEJA <remy.meja@inist.fr>
2017-04-30 22:06:26 +0200

    Adding some unit test for getAllComponentImage

--- tesseract-api/test/octachore-test.cpp
+++ tesseract-api/test/octachore-test.cpp
@@ -4,10 +4,19 @@
 #include "octachore.hpp"
 #include <boost/filesystem.hpp>
 
-std::string imagePath{"lorem-ipsum-mini.tif"};
 std::string currentPath = boost::filesystem::current_path().string();
-std::string text = Octachore::getAllText(currentPath + "/../test/" + imagePath);
+std::string imagePath{currentPath + "/test/lorem-ipsum-mini.tif"};
+std::string text = Octachore::getAllText(imagePath);
+Boxa* blockBoxes = Octachore::getAllComponentImage(imagePath, tesseract::RIL_BLOCK);
 
 TEST_CASE("getAllText have to return some good text") {
     REQUIRE(text.find("Lorem ipsum dolor sit amet") != std::string::npos);
+}
+
+TEST_CASE("getAllComponentImage should return some block segmentation information") {
+   REQUIRE(blockBoxes->n == 1);
+   REQUIRE(blockBoxes->box[0]->x == 33);
+   REQUIRE(blockBoxes->box[0]->y == 37);
+   REQUIRE(blockBoxes->box[0]->w == 518);
+   REQUIRE(blockBoxes->box[0]->h == 37);
 }
